618c2584d80918239eb75b1154c3fc0f8f9e1dc3
Shivam Hasurkar <64827633+ShivamHasurkar@users.noreply.github.com>
2020-09-11 14:07:20 +0530

    Update app_colors.dart

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color.fromRGBO(10, 186, 181, 1);
+const Color primaryColor = Color.fromRGBO(11, 186, 181, 1);
 //const Color primaryColor = Color(0xFF00BCD4);
 const Color iconColor = Colors.white;
